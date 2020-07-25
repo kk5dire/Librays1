a2e0fa028a2ffcc89be62b00be1b09a159aaca14
kk5dire <damianswan270number2@gmail.com>
2020-07-25 17:37:42 -0400

    changed token

--- bot.js
+++ bot.js
@@ -106,4 +106,4 @@ client.on('messageUpdate', (oldMsg, newMsg) => {
 
 //log in to Discord with the bot token in config.json
 //go to https://discordapp.com/developers/applications to get your own bot
-client.login('NzM2Njk0MzE5NTgyNTQzOTIy.XxyiDA.s1oo2YQV8W1sbOLXNJg13EpTaA');
+client.login('NzM2Njk0MzE5NTgyNTQzOTIy.XxyiDA.xSx7sbLgs5hSJOZFNalv91KbiWU');
