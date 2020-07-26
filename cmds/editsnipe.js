1b6f6f60c4130526655737e49e62c1b6b08c4e30
kk5dire <damianswan270number2@gmail.com>
2020-07-26 16:49:39 -0400

    renable editsnipe.js

--- cmds/editsnipe.js
+++ cmds/editsnipe.js
@@ -1,4 +1,4 @@
-/* const tools = require('../tools');
+\ const tools = require('../tools');
 
 module.exports = {
     name: 'editsnipe',
@@ -29,4 +29,3 @@ module.exports = {
         }
     },
 };
-*/
