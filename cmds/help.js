fe5f39327091681f8ce0b774c50527a5707e1c2f
kk5dire <damianswan270number2@gmail.com>
2020-07-26 18:33:39 -0400

    help

--- cmds/help.js
+++ cmds/help.js
@@ -5,12 +5,12 @@ module.exports = {
     name: 'help',
     aliases: ['?', 'commands', 'cmds'],
     description: 'Shows available commands or command info',
-    usage: '[+print] [cmd]\n\n+print: Send to this channel instead of DM.',
+    usage: '[-p] [cmd]\n\n-p: Send to this channel instead of DM.',
     async execute(message, args) {
         try {
             //determine target by whether "-p" was added
             let target;
-            if (args[0] == '+print') {
+            if (args[0] == '-p') {
                 args.shift();
                 target = message.channel;
             } else {
@@ -22,9 +22,9 @@ module.exports = {
             if (!args.length) {
                 const mapFunction = command => `\`${command.name}\`   ${command.description} ${command.nsfw ? '\\🔞 ' : ''}${command.ownerOnly ? '\\🔒 ' : ''}${command.image ? '\\🖼️ ' : ''}`;
                 embed.setColor(0x7289DA)
-                .setTitle(':information_source:  Commands')
+                .setTitle('<:mdHelp:568466408548335650> Commands')
                 .setDescription(message.client.commands.map(mapFunction).join('\n'))
-                .setFooter('🔒 = bot owner only   🖼️ = image command ');
+                .setFooter('🔞 = nsfw   🔒 = bot owner only   🖼️ = image command');
             } else {
                 //otherwise show info on one command
                 const name = args[0].toLowerCase();
@@ -32,7 +32,7 @@ module.exports = {
                     message.client.commands.find(c => c.aliases && c.aliases.includes(name));
                 if (!command) throw "command not found";
                 embed.setColor(0x7289DA)
-                .setTitle(`:information_source:  Command ${command.name}`)
+                .setTitle(`<:mdHelp:568466408548335650> Command ${command.name}`)
                 .setDescription(`[Command source](https://github.com/gtrxAC/gtrxBot2/tree/master/cmds/${command.name}.js)\`\`\`${command.description}\n`+
                 (command.usage ? `   Usage: ${command.name} ${command.usage}\n` : `   Usage: ${command.name}\n`)+
                 (command.aliases ? ` Aliases: ${command.aliases.join(', ')}\n` : '')+
@@ -42,7 +42,7 @@ module.exports = {
             tools.sendEmbed(target, embed).then(() => {
                 //if the user needs to check DMs, send a confirmation
                 if (target == message.channel || message.channel.type == 'dm') return;
-                const embed = tools.makeEmbed(':speech_left:  DM sent!', 'Use +print to send to this channel instead.');
+                const embed = tools.makeEmbed('<:mdChat:568466407713407001> DM sent!', 'Use -p to send to this channel instead.');
                 tools.sendEmbed(message.channel, embed);
             }).catch((error) => {
                 throw `can't send DM, do you have DMs disabled?\n${error}`;
