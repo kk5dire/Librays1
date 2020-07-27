6ceb441b1840941c7642138ce1ac3bc249baacce
kk5dire <damianswan270number2@gmail.com>
2020-07-27 19:59:48 -0400

    add timer.js

---
+++ cmds/timer.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'timer',
+    aliases: [],
+    description: 'create a timer that edits in the set amount of time (in seconds)',
+    usage: '-timer 10s',
+    nsfw: false,
+    image: false,
+    args: true,
+    guildOnly: false,
+    ownerOnly: false,
+    requires: '',
+    cooldown: 0,
+    minArgs: 1,
+    maxArgs: 2,
+    async execute(message, args) {
+        try {
+            let msg;
+            bot.on("message", async message => {
+              // Send message & Store reference to the message
+              msg = await message.channel.send(`:timer: | Timer for **${args}**`);
+              setTimeout(() => {
+                // Edit msg 20 seconds later
+                msg.edit(':alarm_clock: | ***`This timer is up`***');
+              }, args);
+            });
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
