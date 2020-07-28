a3faa6c5507f0e9d7215bf422fa338fc7f39d572
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-07-28 11:47:03 -0400

    Create soup.js

---
+++ cmds/soup.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'soup',
+    aliases: [],
+    description: 'tells the truth about soupyg',
+    usage: '-soup',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+    requires: '',
+    cooldown: 0,
+    minArgs: 0,
+    maxArgs: X,
+    async execute(message, args) {
+        try {
+message.channel.send("you mean the simp"?);
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
