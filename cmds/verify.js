125b6bcd64befbc4942304a8516b3117105a4aca
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-13 23:04:43 -0400

    Create verify.js

---
+++ cmds/verify.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'verify',
+    aliases: [veri],
+    description: 'verify your user and enter the testing server',
+    usage: '-ver',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+    async execute(message, args) {
+        try {
+let role = message.guild.roles.find(r => r.name === "testbot");
+let member = message.member;
+
+//adds the role
+member.roles.add(role)
+
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
