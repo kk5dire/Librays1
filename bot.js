eb28c08f315c6fab71419efba644927ba6e65d80
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-14 03:03:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ const activities_list = [
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        cleint.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
     
    
