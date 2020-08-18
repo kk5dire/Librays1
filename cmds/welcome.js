624db1fa60746d151798d4ff689d05b6bf2dd88a
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-18 18:08:07 -0400

    fix typo in welcome.js

--- cmds/welcome.js
+++ cmds/welcome.js
@@ -20,7 +20,7 @@ module.exports = {
           
           const embed = new Discord.MessageEmbed()
         .setColor(0x7289DA)
-        .setTitle(`Welcome to the pepe planet development serve`)
+        .setTitle(`Welcome to the pepe planet development server`)
         .setDescription(`Welcome ${target}! This is the server where we will stress test the bots before we release them to the public`)
         .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/waving-hand-sign_1f44b.png')
         .setFooter('Pepe planet Development');
