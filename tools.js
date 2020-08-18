3467c1ddbfaab0c287dbe648d6d80e03c10eaf58
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-17 22:46:57 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -10,7 +10,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setColor(0x7289DA)
         .setTitle('<a:NO:737449203726549122>  A Error Occured While Doing The Action')
-        .setDescription(`${err}`)
+        .setDescription(`${err} https://www.youtube.com/watch?v=5BZLz21ZS_Y`)
         .setFooter('https://www.youtube.com/watch?v=5BZLz21ZS_Y');
         const outMsg = await this.sendEmbed(message.channel, embed);
         outMsg.delete({timeout: 10000});
