f2acee012a1e5765572f59e77a96ce55af67ffa2
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-18 18:38:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,6 +83,17 @@ const handleCommand = (message) => {
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 }
 
+client.on('guildMemberAdd', member => {
+    const channel = client.channels.cache.get('736595341847298158');
+      const embedm = new Discord.MessageEmbed()
+        .setColor(0x7289DA)
+        .setTitle(`Welcome to the pepe planet development server`)
+        .setDescription(`Welcome ${user}! This is the server where we will stress test the bots before we release them to the public`)
+        .setThumbnail('https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/waving-hand-sign_1f44b.png')
+        .setFooter('Pepe planet Development');
+   channel.send(embedm);
+    member.send(`Welcome to pepe planet development ${user} type \`-verify\` in <#737028828584476702> to get started!`);
+});
 // Main status 
   client.on('ready', () => {
     setInterval(() => {
