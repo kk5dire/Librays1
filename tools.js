d84129124c0b1c8e7ba2beaccfbd6bf770d560c4
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-17 22:51:43 -0400

    Update tools.js

--- tools.js
+++ tools.js
@@ -12,7 +12,9 @@ module.exports = {
         .setTitle('<a:NO:737449203726549122>  A Error Occured While Doing The Action')
         .setDescription(`${err}`)
         .setFooter('https://www.youtube.com/watch?v=5BZLz21ZS_Y');
-        message.channel.send('https://www.youtube.com/watch?v=5BZLz21ZS_Y');
+        message.channel.send('https://youtu.be/5BZLz21ZS_Y?t=6');
+         outMsg.delete({timeout: 10000});
+         if(message.channel.type == 'text') message.delete({timeout: 10000});
         const outMsg = await this.sendEmbed(message.channel, embed);
         outMsg.delete({timeout: 10000});
         if(message.channel.type == 'text') message.delete({timeout: 10000});
