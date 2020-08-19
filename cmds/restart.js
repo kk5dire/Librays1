6eee8899e2da1390014bdd2668463f66ad3eae4d
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-19 00:05:25 -0400

    Update restart.js

--- cmds/restart.js
+++ cmds/restart.js
@@ -13,6 +13,12 @@ module.exports = {
     ownerOnly: true,
     async execute(message, args) {
         try {
+            const embed = new Discord.MessageEmbed()
+        .setColor(0x7289DA)
+        .setTitle(`Restarting`)
+        .setDescription(`<:AYS_info:737448988437118977> The Bot is currently restarting , stand by`)
+   member.send(embed);
+    console.log(`restating the bot via/cmd`);
 process.exit();
         } catch (err) {
             return tools.errorMessage(message, err);
