ef5c16788550ef69f8f01c60d41d6501ef8362f1
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-20 23:51:50 -0400

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const tools = require('../tools');
-const client = new Discord.Client();
+
 
 module.exports = {
     name: 'ping',
     aliases: ['pong'],
     description: "Check the connection ping to Discord's servers",
     cooldown: 5,
-    async execute(message, args) {
+    async execute(client, message, args) {
         try {
             const messageSendStart = Number(new Date());
             const reply = await message.channel.send(":alarm_clock:   Checking ping... `message.edit`")
@@ -27,7 +27,7 @@ module.exports = {
             const messageDeletePing = Number(new Date()) - messageDeleteStart;
             const websocketPing = Math.round(client.ping)
             const embed = tools.makeEmbed(':alarm_clock:   Ping statisitics', `Send ${messageSendPing} ms\n`+
-            ` Edit: ${messageEditPing} ms\nReact: ${messageReactPing} ms\nDelete: ${messageDeletePing} ms\n**Websocket ping:** ${websocketPing}ms\n`+
+            ` Edit: ${messageEditPing} ms\nReact: ${messageReactPing} ms\nDelete: ${messageDeletePing} ms\nWebsocket ping: ${websocketPing}ms\n`+
             `\n\nTotal : ${messageSendPing+messageEditPing+messageReactPing+messageDeletePing} ms\n`, null, message.channel );
         } catch (err) {
             return tools.errorMessage(message, err);
