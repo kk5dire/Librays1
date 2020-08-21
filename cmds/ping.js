491a61b816566d05c387e262917e51709b0f670b
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-20 23:54:52 -0400

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -27,7 +27,7 @@ module.exports = {
             const messageDeletePing = Number(new Date()) - messageDeleteStart;
             const websocketPing = Math.round(client.ping)
             const embed = tools.makeEmbed(':alarm_clock:   Ping statisitics', `Send ${messageSendPing} ms\n`+
-            ` Edit: ${messageEditPing} ms\nReact: ${messageReactPing} ms\nDelete: ${messageDeletePing} ms\nWebsocket ping: ${websocketPing}ms\n`+
+            ` Edit: ${messageEditPing} ms\nReact: ${messageReactPing} ms\nDelete: ${messageDeletePing} ms\nWebsocket ping: ${websocketPing} ms\n`+
             `\n\nTotal : ${messageSendPing+messageEditPing+messageReactPing+messageDeletePing} ms\n`, null, message.channel );
         } catch (err) {
             return tools.errorMessage(message, err);
