43ce60292ff8deb7a6da03e97ab9e8a68adc813d
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-22 01:31:53 -0400

    Create scrub.js

---
+++ cmds/scrub.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'scrub',
+    aliases: ['sc'],
+    description: 'scrub the data files and clean up temp data',
+    usage: '-scrub',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: true,
+    async execute(message, args) {
+        try {
+ const reply = await message.channel.send(tools.makeEmbed('<a:scrub_pengy:746601696691617802> Scrubbing the code...'));
+ console.log("scrubbing data");
+      setTimeout(() => {    reply.edit(tools.makeEmbed('<a:check:740915640063492219> Code Scrubbed and clean')) }, 6000);
+      console.log("Data scrub complete");
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
