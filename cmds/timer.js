0053dd11b140abe212548b0a1dff2ea5aa6f7803
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-25 07:50:51 -0400

    Update timer.js

--- cmds/timer.js
+++ cmds/timer.js
@@ -16,7 +16,7 @@ module.exports = {
     async execute(message, args) {
         try {
  const reply = await message.channel.send(tools.makeEmbed('<:AYS_info:737448988437118977>  Timer set!'));
-      setTimeout(() => {    reply.edit(tools.makeEmbed(':alarm_clock: The Timer is up'))
+      setTimeout(() => {    reply.edit(tools.makeEmbed('<a:timerend:747784369778851921> The Timer is up'))
       message.channel.send(`${message.author} your timer for \`${args} ms\` is up!`)
       message.author.send(`The timer you set for \`${args} ms\` is up!`) }, args);
         } catch (err) {
