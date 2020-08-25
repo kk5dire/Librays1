a9ad1a25d12e87544a9d17ba43b5378fb9c2b872
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-25 07:21:18 -0400

    def client meth 1

--- cmds/help.js
+++ cmds/help.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const tools = require('../tools');
-
+const client = new Discord.Client();
 module.exports = {
     name: 'librarys',
     aliases: ['?', 'libs', 'lib'],
