80f6c10333c8e35d12e1011987a48356a0b6c9d9
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-29 20:43:33 -0400

    add final msg

--- cmds/10.js
+++ cmds/10.js
@@ -14,7 +14,7 @@ module.exports = {
     cooldown: 10,
     async execute(message, args) {
         try {
-message.channel.send(1).then((sentMessage) => sentMessage.edit(2)).then((sentMessage) => sentMessage.edit(3)).then((sentMessage) => sentMessage.edit(4)).then((sentMessage) => sentMessage.edit(5)).then((sentMessage) => sentMessage.edit(6)).then((sentMessage) => sentMessage.edit(7)).then((sentMessage) => sentMessage.edit(8)).then((sentMessage) => sentMessage.edit(9)).then((sentMessage) => sentMessage.edit(10));
+message.channel.send(1).then((sentMessage) => sentMessage.edit(2)).then((sentMessage) => sentMessage.edit(3)).then((sentMessage) => sentMessage.edit(4)).then((sentMessage) => sentMessage.edit(5)).then((sentMessage) => sentMessage.edit(6)).then((sentMessage) => sentMessage.edit(7)).then((sentMessage) => sentMessage.edit(8)).then((sentMessage) => sentMessage.edit(9)).then((sentMessage) => sentMessage.edit(10)).then((sentMessage) => sentMessage.edit('Done!'));
         } catch (err) {
             return tools.errorMessage(message, err);
         }
