6878a03efe8f5bcbc1ecd5ff898cf8eea1b734d9
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-29 20:41:40 -0400

    Create 10.js

---
+++ cmds/10.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: '10',
+    aliases: ['ten'],
+    description: 'count to 10 testing message.edit',
+    usage: '',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+    cooldown: 10,
+    async execute(message, args) {
+        try {
+message.channel.send(1).then((sentMessage) => sentMessage.edit(2)).then((sentMessage) => sentMessage.edit(3)).then((sentMessage) => sentMessage.edit(4)).then((sentMessage) => sentMessage.edit(5)).then((sentMessage) => sentMessage.edit(6)).then((sentMessage) => sentMessage.edit(7)).then((sentMessage) => sentMessage.edit(8)).then((sentMessage) => sentMessage.edit(9)).then((sentMessage) => sentMessage.edit(10));
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
