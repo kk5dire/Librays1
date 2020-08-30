3060aa797dd0788c30a17be6059001f7a37914d9
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-08-29 21:42:54 -0400

    Create iypp.js

---
+++ cmds/iypp.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'iypp',
+    aliases: ['ip'],
+    description: 'finds the size of iyrs pp',
+    usage: '',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+    async execute(message, args) {
+        try {
+message.channel.send(tools.makeEmbed("`.`"))
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
