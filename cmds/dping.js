6d4c1f00db943a4ab59f6148a7bf8781690e4e47
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2020-09-02 00:21:17 -0400

    Create dping.js

---
+++ cmds/dping.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+const client = new Discord.Client();
+
+module.exports = {
+    name: 'dping',
+    aliases: ['dpong'],
+    description: "Check the connection ping to Discord's servers delete",
+    cooldown: 5,
+    async execute(message, args) {
+        try {
+            const messageSendStart = Number(new Date());
+            const reply = await message.channel.send(":alarm_clock:   Checking ping... ")
+            const messageSendPing = Number(new Date()) - messageSendStart;
+
+            const messageEditStart = Number(new Date());
+            await reply.edit(":alarm_clock:   Checking ping... ")
+            const messageEditPing = Number(new Date()) - messageEditStart;
+
+            reply.edit(":alarm_clock:  > Checking ping... ")
+            const messageReactStart = Number(new Date());
+            await reply.react('✅')
+            const messageReactPing = Number(new Date()) - messageReactStart;
+            reply.edit(":alarm_clock:  > Checking ping... `Collecting Data`")
+            const messageDeleteStart = Number(new Date());
+             reply.edit(":alarm_clock:  > Checking ping... `almost done`")
+            await reply.delete();
+            const messageDeletePing = Number(new Date()) - messageDeleteStart;
+            const websocketPing = Math.round(client.ping)
+            const embed = tools.makeEmbed(':alarm_clock:   Ping statisitics', `'Delete: ${messageDeletePing} `, null, message.channel );
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
