922d46ef7487ff8237170c628f05e1df9c3280d4
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-01-14 13:39:03 -0500

    Update tools.js

--- tools.js
+++ tools.js
@@ -42,7 +42,7 @@ module.exports = {
     sendEmbed(channel, embed) {
         return channel.send(embed).catch(e => {
             channel.send(` >>> **${embed.title}**\n${embed.description}\n${embed.footer ? embed.footer.text+'\n' : ''}`+
-            `  Error sending embed message, make sure I can send them.\n(${e})  `)
+            `  I couldnt send the embed make sure i have permissions.\n(${e})  `)
         })
     }
 }
