04a1f1d1cd3dd3b367d43d9e3c67492f2501db91
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-10 21:22:16 -0500

    Delete hello.js

--- cmds/hello.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const tools = require('../tools');
-const Client = new Discord.Client();
-Client.on("message", msg => {
-    if (msg.content === "hi") {
-        msg.reply("hello")
-    }
-})
