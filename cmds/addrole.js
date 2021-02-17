969dc734fa86346d792470bb744b959c8c480888
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-17 08:05:54 -0500

    Create addrole.js

---
+++ cmds/addrole.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'role',
+    aliases: ['r'],
+    description: 'command_placeholder',
+    usage: '-command_handle',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+       async execute(message, args) {
+        try {
+            let target;
+            if (args[0] == '+iy') {
+              args.shift();
+              target = '811581317882970172';
+          } else if (args[0] == '+no') {
+              target = '811581261893468190';
+          } else {
+              message.channel.send(tools.errorMessage(message, err));
+              break;
+          }
+        
+        // assuming role.id is an actual ID of a valid role:
+        if(message.member.roles.cache.has(target)) { // crew role
+            console.log(`Yay, the author of the message has the role!`);
+          let role = message.guild.roles.cache.get(target); // access role
+          let member = message.member;
+           member.roles.remove(role.id)
+          const embed = tools.makeEmbed('<:AYS_check:737449100009799752> success! , removed the role(s)')
+          message.channel.send(embed);
+          
+         
+           return;
+          } else {
+            console.log(`Nope, noppers, nadda.`);
+          let role = message.guild.roles.cache.get(target); // access role
+          let member = message.member;
+           member.roles.add(role.id)
+          const embed = tools.makeEmbed('<:AYS_check:737449100009799752> success! , gave you the role(s)')
+          message.channel.send(embed);
+          
+         
+          } 
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
