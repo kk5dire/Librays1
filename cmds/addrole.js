c7677b8747a99333b3b60629b63701eb4d5e33ca
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-17 08:38:06 -0500

    Update addrole.js

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -33,7 +33,7 @@ module.exports = {
           const embed = tools.makeEmbed('<:AYS_check:737449100009799752> success! , removed the role(s)')
           message.channel.send(embed);
           embed.delete({timeout: 200000});
-          message.channel.bulkDelete(1)
+          message.channel.bulkDelete(1)({timeout: 200000})
         
           
          
@@ -46,7 +46,7 @@ module.exports = {
           const embed = tools.makeEmbed('<:AYS_check:737449100009799752> success! , gave you the role(s)')
           message.channel.send(embed);
           embed.delete({timeout: 200000});
-          message.channel.bulkDelete(1)
+          message.channel.bulkDelete(1)({timeout: 200000})
           
          
           } 
