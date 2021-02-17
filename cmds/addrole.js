e8c7217cb9df78c48d7a441716e4f3612392af58
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-17 08:15:05 -0500

    Update addrole.js

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -20,12 +20,12 @@ module.exports = {
           } else if (args[0] == '+no') {
               target = '811581261893468190';
           } else {
-              message.channel.send(tools.errorMessage(message, err));
+              message.channel.send(tools.errorMessage(message));
               return;
           }
         
         // assuming role.id is an actual ID of a valid role:
-        if (target === undefined) throw message.channel.send(tools.errorMessage(message, err));
+        if (target === undefined) throw message.channel.send(tools.errorMessage(message));
         if(message.member.roles.cache.has(target)) { // crew role
             console.log(`Yay, the author of the message has the role!`);
           let role = message.guild.roles.cache.get(target); // access role
