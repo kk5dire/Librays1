e2c823ff8471084b5d229917191fe227eecda163
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-17 08:19:58 -0500

    Update addrole.js

--- cmds/addrole.js
+++ cmds/addrole.js
@@ -14,18 +14,17 @@ module.exports = {
        async execute(message, args) {
         try {
             let target;
-            if (args[0] == '+iy') {
+            if (args[0] == '+lib') {
               args.shift();
               target = '811581317882970172';
           } else if (args[0] == '+no') {
               target = '811581261893468190';
           } else {
-              message.channel.send(tools.errorMessage(message));
               return;
           }
         
         // assuming role.id is an actual ID of a valid role:
-        if (target === undefined) throw message.channel.send(tools.errorMessage(message));
+        if (target === undefined) throw message.channel.send(tools.errorMessage('Undefined Paramters'));
         if(message.member.roles.cache.has(target)) { // crew role
             console.log(`Yay, the author of the message has the role!`);
           let role = message.guild.roles.cache.get(target); // access role
