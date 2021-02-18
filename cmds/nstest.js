dc1ae2815360d852f26a0a20af15f5deda0476cb
kkd5dire <52354566+kk5dire.commits@users.noreply.github.com>
2021-02-17 22:06:25 -0500

    Update nstest.js

--- cmds/nstest.js
+++ cmds/nstest.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const tools = require('../tools');
 const cheerio = require('cheerio');
 const request = require('request');
+const url = 'https://www.reddit.com/r/meme/hot/.json?limit=100'
 
 module.exports = {
     name: 'img',
@@ -13,41 +14,55 @@ module.exports = {
     args: true,
     async execute(message, args) {
         try {
-            function image(msg) {
-
-                var options = {
-                    url: "http://results.dogpile.com/serp?qc=images&q=" + args,
-                    method: "GET",
-                    headers: {
-                        "Accept": "text/html",
-                        "User-Agent": "Chrome"
+            https.get(url, (result) => {
+                var body = ''
+                result.on('data', (chunk) => {
+                    body += chunk
+                })
+    
+                result.on('end', () => {
+                    var response = JSON.parse(body)
+                    var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
+    
+                    if (index.post_hint !== 'image') {
+    
+                        var text = index.selftext
+                        const textembed = new Discord.MessageEmbed()
+                            .setTitle(subRedditName)
+                            .setColor(9384170)
+                            .setDescription(`[${title}](${link})\n\n${text}`)
+                            .setURL(`https://reddit.com/${subRedditName}`)
+    
+                        message.channel.send(textembed)
                     }
-                };
-            
-                request(options, function (error, response, responseBody) {
-                    if (err) {
-                        return;
+    
+                    var image = index.preview.images[0].source.url
+                    var title = index.title
+                    var link = 'https://reddit.com' + index.permalink
+                    var subRedditName = index.subreddit_name_prefixed
+    
+                    if (index.post_hint !== 'image') {
+                        const textembed = new Discord.RichEmbed()
+                            .setTitle(subRedditName)
+                            .setColor(9384170)
+                            .setDescription(`[${title}](${link})\n\n${text}`)
+                            .setURL(`https://reddit.com/${subRedditName}`)
+    
+                        message.channel.send(textembed)
                     }
+                    console.log(image);
+                    const imageembed = new Discord.MessageEmbed()
+                        .setTitle(subRedditName)
+                        .setImage(image)
+                        .setColor(9384170)
+                        .setDescription(`[${title}](${link})`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+                    message.channel.send(imageembed)
+                }).on('error', function (e) {
+                    console.log('Got an error: ', e)
+                })
+            })
             
-                    $ = cheerio.load(responseBody);
-            
-            
-                    var links = $(".image a.link");
-            
-                    var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-            
-                    console.log(urls);
-                    if (!urls.length) {
-            
-                        return;
-            
-                    }
-            
-                    //send request
-                    message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
-            
-                });
-            }
         } catch (err) {
             return tools.errorMessage(message, err);
         }
