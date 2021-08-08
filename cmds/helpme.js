b43448982868537092e48feeeb3a2d8083300096
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-08 03:51:02 -0400

    Create helpme.js

---
+++ cmds/helpme.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const tools = require('../tools');
+
+module.exports = {
+    name: 'help',
+    aliases: ['?'],
+    description: 'Help',
+    usage: '-help',
+    nsfw: false,
+    image: false,
+    args: false,
+    guildOnly: false,
+    ownerOnly: false,
+    async execute(message, args) {
+        try {
+        message.channel.send("https://kk5dire.github.io/Librays1/");
+        } catch (err) {
+            return tools.errorMessage(message, err);
+        }
+    },
+};
