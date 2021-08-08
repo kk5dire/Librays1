8895388800d2f84eebcd88476d5b088bcac08804
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-08 02:57:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 //set login token
-const token = 'ODQzNzgyMDk4Mzk2MzgxMTk0.YKI3UA.raPJMiEFFblSGGddNHcY9390-Aw'
+const token = AuthDetails.bot_token;
 //load required modules
 const fs = require('fs');
 const Discord = require('discord.js');
