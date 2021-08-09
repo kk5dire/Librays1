ba3267fffacf6d43e6a2e945ee9762692869ed96
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-09 04:18:41 -0400

    EM

--- cmds/timer.js
+++ cmds/timer.js
@@ -15,9 +15,9 @@ module.exports = {
     maxArgs: 1,
     async execute(message, args) {
         try {
- const reply = await message.channel.send(tools.makeEmbed('<:AYS_info:737448988437118977>  Timer set!'));
+ const reply = await message.channel.send(tools.makeEmbed('<:okmuda:821852023053484092>  Timer set!'));
             console.log(`a timer has been started for ${args} by ${message.author}`);
-      setTimeout(() => {    reply.edit(tools.makeEmbed('<a:timerend:747784369778851921> The Timer is up'))
+      setTimeout(() => {    reply.edit(tools.makeEmbed('<:resme:827058028816760872> The Timer is up'))
       message.channel.send(`${message.author} your timer for \`${args} ms\` is up!`)
                          console.log(`the timer set by ${message.author} for ${args} is up`);
       message.author.send(`The timer you set for \`${args} ms\` is up!`) }, args);
