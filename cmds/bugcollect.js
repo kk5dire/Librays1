9fb44d8d3f13663cefb4e2bf301ec14ed8bd7124
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-09 04:20:28 -0400

    EM

--- cmds/bugcollect.js
+++ cmds/bugcollect.js
@@ -13,9 +13,9 @@ module.exports = {
     ownerOnly: true,
     async execute(message, args) {
         try {
- const reply = await message.channel.send('<:checklist:740365693442064414> Generating error report');
+ const reply = await message.channel.send('<:okmuda:821852023053484092> Generating error report');
  console.log("Generating bug report");
-      setTimeout(() => {    reply.edit("<a:check:740915640063492219> Bug report generated") }, 2000);
+      setTimeout(() => {    reply.edit("<:resme:827058028816760872> Bug report generated") }, 2000);
       console.log("Bug report sent to local file");
         } catch (err) {
             return tools.errorMessage(message, err);
