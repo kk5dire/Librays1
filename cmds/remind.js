679a7491e10e34baafdc3c3aee4460ccf3facaed
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-09 04:19:06 -0400

    EM

--- cmds/remind.js
+++ cmds/remind.js
@@ -15,9 +15,9 @@ module.exports = {
     maxArgs: 2,
     async execute(message, args) {
         try {
- const reply = await message.channel.send(tools.makeEmbed('<:AYS_info:737448988437118977>  Reminder set!'));
+ const reply = await message.channel.send(tools.makeEmbed('<:okmuda:821852023053484092>  Reminder set!'));
             console.log(`a remind has been started for ${args[0]} by ${message.author} with the message ${args[1]} `);
-      setTimeout(() => {    reply.edit(tools.makeEmbed('<:checklist:740365693442064414> The Reminder is up'))
+      setTimeout(() => {    reply.edit(tools.makeEmbed('<:resme:827058028816760872> The Reminder is up'))
       message.channel.send(`${message.author} i was told to remind you about \`${args[1]}\` \`${args[0]}\`ms ago`)
         console.log(`the remind set by ${message.author} for \`${args}\` is up with the message \`${args[1]}\``);
       message.author.send(`You told me to remind you for ${args[1]}  ${args[0]}ms ago`) }, args[0]);
