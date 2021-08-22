c5fa8653e73d13eb7c6111ff164c80b9e8b2a5cb
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-22 01:35:49 -0400

    Fix quotation issues and return tools

--- cmds/bio.js
+++ cmds/bio.js
@@ -13,8 +13,10 @@ module.exports = {
     ownerOnly: false,
     async execute(message, args) {
         try {
+    const bio = new Discord.MessageEmbed()
     .setTitle('Bio overflow')
-    .setDescription("This bot is maintained by ***kk5dire*** for more info open my profile\n'-help' for docs "-sources" for sources '-about' for the bots history\nhttps://anotherkk5dire.web.app \ncontact@anotherkk5dire.web.app for issues\n Used in with: *https://kk5.page.link/la4* \nThis bot contains outdated code using discord.js 11 and may contain bugs use the above link to report crashes and bugs")
+    .setDescription("This bot is maintained by ***kk5dire*** for more info open my profile\n'-help' for docs '-sources' for sources '-about' for the bots history\nhttps://anotherkk5dire.web.app \ncontact@anotherkk5dire.web.app for issues\n Used in with: *https://kk5.page.link/la4* \nThis bot contains outdated code using discord.js 11 and may contain bugs use the above link to report crashes and bugs")
+    tools.sendEmbed(message.channel, bio)
         } catch (err) {
             return tools.errorMessage(message, err);
         }
