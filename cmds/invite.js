71165466382d54061918e9a931cc91ed44b70eb8
kkd5dire <52354566+kk5dire@users.noreply.github.com>
2021-08-22 02:00:32 -0400

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -11,7 +11,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setColor(0x7289DA)
             .setTitle(' Link')
-            .setDescription("[Click me](https://discord.com/api/oauth2/authorize?client_id=843782098396381194&permissions=0&scope=bot)\n[Click me](https://kk5dire.github.io/Librays1/)")
+            .setDescription("[Click me](https://discord.com/api/oauth2/authorize?client_id=843782098396381194&permissions=0&redirect_uri=https%3A%2F%2Fkk5dire.github.io%2FLibrays1%2F&scope=bot)")
             tools.sendEmbed(message.channel, embed);
         } catch (err) {
             return tools.errorMessage(message, err);
